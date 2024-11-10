5510a7ef1183013f39f52e96948f411bbd4e3a24
Zach Sisco <zach@zsisco.net>
2024-11-09 21:44:59 -0800

    genericize lowering test tcl test

--- abstract-mem/lowering_tests.py
+++ abstract-mem/lowering_tests.py
@@ -15,20 +15,19 @@ def test_vivado_bram(height_log2, width, num_read_ports=1, num_write_ports=0):
     rdatas = []
     for i in range(num_read_ports):
         raddrs.append(pyrtl.Input(addr_width, 'raddr{i}'))
-        rdatas.append(pyrtl.WireVector(val_width, 'rdata{i}'))
+        rdatas.append(pyrtl.Output(val_width, 'rdata{i}'))
 
     waddrs = []
+    wdatas = []
     w_ens = []
     for i in range(num_write_ports):
         waddrs.append(pyrtl.Input(addr_width, 'waddr{i}'))
+        wdatas.append(pyrtl.Input(addr_width, 'wdatas{i}'))
         w_ens.append(pyrtl.Input(1, 'w_en{i}'))
         
-    inc = pyrtl.WireVector(val_width, 'inc')
-    inc <<= rdatas[0] + 1
-
     write_port = None
     if num_write_ports > 0:
-        write_port=AbstractMem.WritePort(waddrs[0], inc, w_ens[0])
+        write_port=AbstractMem.WritePort(waddrs[0], wdatas[0], w_ens[0])
 
     mem = AbstractMem(
             width=val_width,
@@ -37,12 +36,10 @@ def test_vivado_bram(height_log2, width, num_read_ports=1, num_write_ports=0):
             read_ports=[AbstractMem.ReadPort(raddrs[i], rdatas[i], pyrtl.Const(1,bitwidth=1)) for i in range(num_read_ports)],
             write_port=write_port,
             )
+
     mem.to_pyrtl(pyrtl.working_block())
-
-    data_o = pyrtl.Output(val_width, 'data_o')
-    data_o <<= inc
-
     pyrtl.working_block().sanity_check()
+
     return mem.to_vivado_bram_tcl()
 
 if __name__ == '__main__':
