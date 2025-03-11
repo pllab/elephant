a00129ee485154efa126abba8266512b51d7171d
Dan Petrisko <petrisko@cs.washington.edu>
2025-03-11 13:41:17 -0700

    Fixing async synth memories in pickles

--- memory-decomp/eval/pickle_blackparrot.sh
+++ memory-decomp/eval/pickle_blackparrot.sh
@@ -19,12 +19,36 @@ DESIGNS=" \
     bp_me_cache_slice
 "
 
+echo "Pickling..."
 for design in ${DESIGNS}; do
+    echo "\t*${design}"
     cat ${COMMON_ROOT}/bsg_defines.sv \
         ${COMMON_ROOT}/*synth*.sv \
         ${COMMON_ROOT}/${design}.sv2v.v \
         | sed "s#\`include#//\`include#g" \
         | sed "s#\`BSG_ABSTRACT_MODULE#//\`BSG_ABSTRACT_MODULE#g" \
-            > ${MICROBENCHMARKS_ROOT}/${design}.pickle.v
+        > ${MICROBENCHMARKS_ROOT}/${design}.pickle.v
+done
+
+echo "Getting JSON/BLIF..."
+for design in ${DESIGNS}; do
+    echo "\t*${design}"
+    yosys -p "read_verilog -sv ${MICROBENCHMARKS_ROOT}/${design}.pickle.v;
+        synth -flatten -top ${design}; \
+        dfflegalize -cell \$_DFFE_PP_ 01; \
+        proc; \
+        write_json ${MICROBENCHMARKS_ROOT}/${design}.json; \
+        write_blif ${MICROBENCHMARKS_ROOT}/${design}.blif; \
+        tee -a ${MICROBENCHMARKS_ROOT}/${design}.dat stat -width; \
+    "
+done
+
+echo "Zipping up results for..."
+for design in ${DESIGNS}; do
+    echo "\t*${design}"
+    gzip -f ${MICROBENCHMARKS_ROOT}/${design}.pickle.v
+    gzip -f ${MICROBENCHMARKS_ROOT}/${design}.json
+    gzip -f ${MICROBENCHMARKS_ROOT}/${design}.blif
+    gzip -f ${MICROBENCHMARKS_ROOT}/${design}.dat
 done
 
