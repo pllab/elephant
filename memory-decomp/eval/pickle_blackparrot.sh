75f288992dec983e74e896f3075bc79ca4003d60
Dan Petrisko <petrisko@cs.washington.edu>
2025-03-11 16:04:27 -0700

    Fix for infinite elaboration

--- memory-decomp/eval/pickle_blackparrot.sh
+++ memory-decomp/eval/pickle_blackparrot.sh
@@ -19,30 +19,44 @@ DESIGNS=" \
     bp_me_cache_slice
 "
 
-echo "Pickling..."
-for design in ${DESIGNS}; do
-    echo "\t*${design}"
-    cat ${COMMON_ROOT}/bsg_defines.sv \
-        ${COMMON_ROOT}/*synth*.sv \
-        ${COMMON_ROOT}/${design}.sv2v.v \
-        | sed "s#\`include#//\`include#g" \
-        | sed "s#\`BSG_ABSTRACT_MODULE#//\`BSG_ABSTRACT_MODULE#g" \
-        > ${MICROBENCHMARKS_ROOT}/${design}.pickle.v
-done
+#DO_PICKLE=0
+DO_PICKLE=1
+if [ -n "${DO_PICKLE}" ]; then
+    echo "[BSG-INFO] Pickling..."
+    for design in ${DESIGNS}; do
+        echo "\t*${design}"
+        cat ${COMMON_ROOT}/bsg_defines.sv \
+            ${COMMON_ROOT}/bsg_mem*.sv \
+            ${COMMON_ROOT}/${design}.sv2v.v \
+            | sed "s#\`include#//\`include#g" \
+            | sed "s#\`BSG_ABSTRACT_MODULE#//\`BSG_ABSTRACT_MODULE#g" \
+            > ${MICROBENCHMARKS_ROOT}/${design}.pickle.v
+    done
+else
+    echo "[BSG-WARNING] Skipping pickling..."
+fi
 
-echo "Getting JSON/BLIF..."
-for design in ${DESIGNS}; do
-    echo "\t*${design}"
-    yosys -p "read_verilog -sv ${MICROBENCHMARKS_ROOT}/${design}.pickle.v;
-        synth -flatten -top ${design}; \
-        dfflegalize -cell \$_DFFE_PP_ 01; \
-        proc; \
-        write_json ${MICROBENCHMARKS_ROOT}/${design}.json; \
-        write_blif ${MICROBENCHMARKS_ROOT}/${design}.blif; \
-        tee -a ${MICROBENCHMARKS_ROOT}/${design}.dat stat -width; \
-    "
-done
+#DO_JSON=
+DO_JSON=1
+if [ -n "${DO_JSON}" ]; then
+    echo "[BSG-INFO] Getting JSON/BLIF..."
+    for design in ${DESIGNS}; do
+        echo "\t*${design}"
+        yosys -g -p "read_verilog -sv ${MICROBENCHMARKS_ROOT}/${design}.pickle.v;
+            synth -flatten -top ${design}; \
+            dfflegalize -cell \$_DFFE_PP_ 01; \
+            proc; \
+            write_json ${MICROBENCHMARKS_ROOT}/${design}.json; \
+            write_blif ${MICROBENCHMARKS_ROOT}/${design}.blif; \
+            tee -a ${MICROBENCHMARKS_ROOT}/${design}.dat stat -width; \
+        "
+    done
+else
+    echo "[BSG-WARNING] Skipping json..."
+fi
 
+#DO_ZIP=
+DO_ZIP=1
 echo "Zipping up results for..."
 for design in ${DESIGNS}; do
     echo "\t*${design}"
