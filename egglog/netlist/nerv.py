0f497a3e473b7eac089b4d9de6f3c39a22f14acf
Zach Sisco <zach@zsisco.net>
2025-04-15 11:08:19 -0700

    remove old files

--- egglog/netlist/nerv.py
+++
@@ -1,9 +0,0 @@
-import pyrtl
-import pyrtl_to_egglog
-
-with open('nerv.blif', "r") as f:
-    s = f.read()
-
-pyrtl.importexport.input_from_blif(s, merge_io_vectors=False, clock_name='clock', top_model='nerv')
-
-pyrtl_to_egglog.to_egglog(NAME='nerv', synth=True, opt=True)
