f3af33b101a393dbeb48887ac951246eb3dd9586
Gus Smith <guscomps@gmail.com>
2025-04-15 21:28:39 -0700

    Make test script a bit more portable

--- egglog/run_all_tests.sh
+++ egglog/run_all_tests.sh
@@ -1,33 +1,103 @@
 #!/bin/bash
 
+# Get the directory of this script.
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+####################### VARIABLES USER CAN OVERRIDE ############################
+# The egglog command to use.
+EGGLOG=${EGGLOG:-egglog}
+# SVG directory. Will be relative to the test directory.
+SVG_DIR=${SVG_DIR:-$SCRIPT_DIR/svg}
+################################################################################
+
+# Exit on error.
+set -e
+# Exit on error in a pipeline.
+set -o pipefail
+
+# Exit routine.
+cleanup_and_exit() {
+    prev_return_code=$?
+
+    # Cleanup files.
+    rm -f $stdout_file $stderr_file
+
+    # If there was an error or if the previous command failed, print the error message.
+
+    if [ "$ERROR" = true ] || [ $prev_return_code != 0 ]; then
+        >&2 echo ""
+        >&2 echo "Tests did not run successfully!"
+    else
+        >&2 echo ""
+        >&2 echo "All tests ran successfully!"
+    fi
+
+    cd $OLD_PWD
+
+    code=${1:-$prev_return_code}
+    exit $code
+}
+trap cleanup_and_exit EXIT
+
+# Create the SVG directory if it doesn't exist.
+if [ ! -d "$SVG_DIR" ]; then
+    mkdir -p "$SVG_DIR"
+fi
+
 # Directory containing the test files
-TEST_DIR="/Users/julian/Documents/UniTU/ArchLab/elephant_egglog/tests"
+TESTS_DIR="$SCRIPT_DIR/tests"
 ERROR=false
 
+# Make sure we're running in the script dir if we're not there already. This is
+# unfortunately needed because the include commands in egglog are relative to
+# the current working directory.
+OLD_PWD=$(pwd)
+cd $SCRIPT_DIR
+# Ensure elephant.egg is in the current directory.
+if [ ! -f elephant.egg ]; then
+    >&2 echo "elephant.egg not found in $SCRIPT_DIR"
+    cleanup_and_exit 1
+fi
+
+# Check if the egglog command is available, error if it isn't. User can override
+# the egglog command by setting the EGGLOG environment variable.
+if ! command -v $EGGLOG 2>&1 >/dev/null
+then
+    >&2 echo "$EGGLOG could not be found. Please install egglog with `cargo install egglog`, or set the EGGLOG environment variable to the path of the egglog binary."
+    cleanup_and_exit 1
+fi
+>&2 echo "egglog command: $EGGLOG"
+
 # Loop through all files in the test directory
-for file in "$TEST_DIR"/*; do
+for file in "$TESTS_DIR"/*; do
+    stdout_file=$(mktemp)
+    stderr_file=$(mktemp)
     # Check if the file does not start with an underscore
     if [[ $(basename "$file") != _* ]] && [[ $(basename "$file") != note.md ]]; then
-        # Run the egglog command with the file and convert to svg, suppress output
-        if egglog "$file" --to-svg > /dev/null 2>&1; then
-            # Run the svg_conversion.sh script with png as an argument, suppress output
-            if ./svg_conversion.sh png > /dev/null 2>&1; then
-                echo "Success: $(basename "$file")"
+        # Run the egglog command with the file and convert to svg. Note: Deleted
+        # the old svg_conversion.sh script that converted the SVG to PNG;
+        # if needed, can be rewritten.
+        if egglog "$file" --to-svg > $stdout_file 2> $stderr_file; then
+            # Move the SVG file to the SVG directory.
+            svg_file=${file%.egg}.svg
+            if [ -f "$svg_file" ]; then
+                mv "$svg_file" "$SVG_DIR"
+                >&2 echo "Success: $(basename "$file")"
             else
-                echo "Error: $(basename "$file") during svg conversion"
+                >&2 echo "Error: $(basename "$file") did not produce an SVG file"
                 ERROR=true
             fi
+
         else
-            echo "Error: $(basename "$file") during egglog execution"
+            >&2 echo "Error: $(basename "$file") during egglog execution"
+            >&2 echo 
+            >&2 echo "stdout:"
+            >&2 cat $stdout_file
+            >&2 echo 
+            >&2 echo "stderr:"
+            >&2 cat $stderr_file
+
             ERROR=true
         fi
     fi
 done
-
-if [ "$ERROR" = true ]; then
-    echo ""
-    echo "Not all tests ran successfully!"
-else
-    echo ""
-    echo "All tests ran successfully!"
-fi
