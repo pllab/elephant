920eb31b21f65b48c3d7c9404712d86bdf4becea
Zach Sisco <zach@zsisco.net>
2025-04-23 15:58:08 -0700

    remove async logic from sparc_ffu bram 1rw

--- database/eval/opdb/verilog/sparc_ffu.pickle.v
+++ database/eval/opdb/verilog/sparc_ffu.pickle.v
@@ -404,9 +404,9 @@ reg                             rw_conflict_r;
 
 
 
-reg   [ADDR_WIDTH-1:0    ]      WRITE_ADDRESS_REG_muxed;
-reg                            bram_write_en_muxed;
-reg  [DATA_WIDTH-1:0    ]      bram_data_in_muxed;
+// reg   [ADDR_WIDTH-1:0    ]      WRITE_ADDRESS_REG_muxed;
+// reg                            bram_write_en_muxed;
+// reg  [DATA_WIDTH-1:0    ]      bram_data_in_muxed;
 
 assign write_en   = CE & (RDWEN == 1'b0);
 assign read_en    = CE & (RDWEN == 1'b1);
@@ -448,8 +448,10 @@ assign DOUT          = ren_r ? up_to_date_data : DOUT_r;
 reg [DATA_WIDTH-1:0] ram [DEPTH-1:0];
 // reg [%d-1:0] bram_data_out;
 always @(posedge MEMCLK) begin
-  if (bram_write_en_muxed) begin
-      ram[WRITE_ADDRESS_REG_muxed] <= bram_data_in_muxed;
+  //if (bram_write_en_muxed) begin
+  if (bram_wen) begin
+      ram[A_r] <= bram_data_in;
+      // ram[WRITE_ADDRESS_REG_muxed] <= bram_data_in_muxed;
   end
   if (bram_ren) begin
     bram_data_out <= ram[A];
@@ -460,50 +462,50 @@ end
  // undefined by default
 
 /* BIST logic for resetting RAM content to 0s on reset*/
-localparam INIT_STATE = 1'd0;
-localparam DONE_STATE  = 1'd1;
+// localparam INIT_STATE = 1'd0;
+// localparam DONE_STATE  = 1'd1;
+//
+// reg [ADDR_WIDTH-1:0] bist_index;
+// reg [ADDR_WIDTH-1:0] bist_index_next;
+// reg init_done;
+// reg init_done_next;
 
-reg [ADDR_WIDTH-1:0] bist_index;
-reg [ADDR_WIDTH-1:0] bist_index_next;
-reg init_done;
-reg init_done_next;
+// always @ (posedge MEMCLK)
+// begin
+//    if (!RESET_N)
+//    begin
+//       bist_index <= 0;
+//       init_done <= 0;
+//    end
+//    else
+//    begin
+//       bist_index <= bist_index_next;
+//       init_done <= init_done_next;
+//    end
+// end
 
-always @ (posedge MEMCLK)
-begin
-   if (!RESET_N)
-   begin
-      bist_index <= 0;
-      init_done <= 0;
-   end
-   else
-   begin
-      bist_index <= bist_index_next;
-      init_done <= init_done_next;
-   end
-end
-
-always @ *
-begin
-   bist_index_next = init_done ? bist_index : bist_index + 1;
-   init_done_next = ((|(~bist_index)) == 0) | init_done;
-end
+// always @ *
+// begin
+//    bist_index_next = init_done ? bist_index : bist_index + 1;
+//    init_done_next = ((|(~bist_index)) == 0) | init_done;
+// end
 
 // MUX for BIST
-always @ *
-begin
-   if (!init_done)
-   begin
-      WRITE_ADDRESS_REG_muxed = bist_index;
-      bram_write_en_muxed = 1'b1;
-      bram_data_in_muxed = {DATA_WIDTH{1'b0}};
-   end
-   else
-   begin
-      WRITE_ADDRESS_REG_muxed = A_r;
-      bram_write_en_muxed = bram_wen;
-      bram_data_in_muxed = bram_data_in;
-   end
-end
+// always @ *
+// begin
+//    if (!init_done)
+//    begin
+//       WRITE_ADDRESS_REG_muxed = bist_index;
+//       bram_write_en_muxed = 1'b1;
+//       bram_data_in_muxed = {DATA_WIDTH{1'b0}};
+//    end
+//    else
+//    begin
+//       WRITE_ADDRESS_REG_muxed = A_r;
+//       bram_write_en_muxed = bram_wen;
+//       bram_data_in_muxed = bram_data_in;
+//    end
+// end
 
 
 
