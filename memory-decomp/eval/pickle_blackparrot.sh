f65b710223813414d85a9b894e07c9abd0f7f55b
Dan Ruelas-Petrisko <petrisko@cs.washington.edu>
2025-08-04 02:24:55 -0700

    Adding blackparrot results

--- memory-decomp/eval/pickle_blackparrot.sh
+++ memory-decomp/eval/pickle_blackparrot.sh
@@ -2,40 +2,32 @@
 
 set -e
 
-MICROBENCHMARKS_ROOT=$PWD/microbenchmarks
-INPUTS_ROOT=$MICROBENCHMARKS_ROOT/blackparrot/inputs
-WORK_ROOT=$MICROBENCHMARKS_ROOT/blackparrot/work
-OUTPUTS_ROOT=$MICROBENCHMARKS_ROOT/blackparrot/outputs
+export PARALLEL_JOBS=8
+#export DO_STRIP=
+export DO_STRIP=1
+#export DO_JSON=
+export DO_JSON=1
+#export DO_ZIP=
+export DO_ZIP=1
 
-DESIGNS=(
-    e_bp_unicore_cfg
-)
-#    e_bp_unicore_megaparrot_cfg
-#    e_bp_unicore_miniparrot_cfg
-#    e_bp_unicore_tinyparrot_cfg
+run_testcase() {
+    MICROBENCHMARKS_ROOT=$PWD/microbenchmarks
+    INPUTS_ROOT=$MICROBENCHMARKS_ROOT/blackparrot/inputs
+    WORK_ROOT=$MICROBENCHMARKS_ROOT/blackparrot/work
+    OUTPUTS_ROOT=$MICROBENCHMARKS_ROOT/blackparrot/outputs
 
-SUBDESIGNS=(
-    bp_fe_icache
-)
-#    bp_be_dcache
-#    bp_be_fp_regfile
-#    bp_be_int_regfile
-
-
-for design in "${DESIGNS[@]}"; do
-for sub in "${SUBDESIGNS[@]}"; do
-    testcase=${design}-${sub}
+    design="$1"
+    sub="$2"
+    testcase="${design}-${sub}"
 
     echo "[BSG-INFO] Running testcase ${testcase}..."
 
-    PICKLE_SV=${INPUTS_ROOT}/inputs/${design}.pickled.sv
+    PICKLE_SV=${INPUTS_ROOT}/${design}.pickled.sv
     STRIPPED_SV=${WORK_ROOT}/${testcase}.stripped.sv
     JSON_FILE=${WORK_ROOT}/${testcase}.json
     BLIF_FILE=${WORK_ROOT}/${testcase}.blif
     DAT_FILE=${WORK_ROOT}/${testcase}.dat
 
-    DO_STRIP=
-    #DO_STRIP=1
     if [ -n "${DO_STRIP}" ]; then
         echo "[BSG-INFO] Stripping pickle"
         python strip_pickle.py ${sub} ${PICKLE_SV} ${STRIPPED_SV}
@@ -43,8 +35,6 @@ for sub in "${SUBDESIGNS[@]}"; do
         sed -i "s#\"inv\"#16#" ${STRIPPED_SV}
     fi
 
-    DO_JSON=
-    #DO_JSON=1
     if [ -n "${DO_JSON}" ]; then
         echo "[BSG-INFO] Getting JSON/BLIF..."
             yosys -g -p "read_verilog -sv ${STRIPPED_SV}; \
@@ -60,17 +50,36 @@ for sub in "${SUBDESIGNS[@]}"; do
         echo "[BSG-WARNING] Skipping json..."
     fi
     
-    #DO_ZIP=
-    DO_ZIP=1
     if [ -n "${DO_ZIP}" ]; then
         echo "Zipping up results for..."
         echo "\t*${design}"
-        gzip ${JSON_FILE}
-        gzip ${BLIF_FILE}
-        gzip ${DAT_FILE}
+        gzip -f ${JSON_FILE} ${BLIF_FILE} ${DAT_FILE}
+        mv ${WORK_ROOT}/*.gz ${OUTPUTS_ROOT}/
     else
         echo "[BSG-WARNING] Skipping zip..."
     fi
-done
-done
+}
+export -f run_testcase
+
+DESIGNS=(
+    e_bp_unicore_tinyparrot_cfg
+    e_bp_unicore_cfg
+    e_bp_unicore_megaparrot_cfg
+    e_bp_unicore_miniparrot_cfg
+    e_bp_unicore_tinyparrot_cfg
+)
+
+SUBDESIGNS=(
+    bp_fe_icache
+    bp_fe_pc_gen
+    bp_be_dcache
+    bp_be_scheduler
+    bp_fe_top
+    bp_be_top
+    bp_core_minimal
+    bp_me_cache_slice
+    bp_unicore
+)
+
+parallel --progress --joblog job.log --results results --jobs ${PARALLEL_JOBS} run_testcase ::: "${DESIGNS[@]}" ::: "${SUBDESIGNS[@]}"
 
