7a83a15106cab0cfcf5f929bcd93b23fbb4e12ad
Zach Sisco <zach@zsisco.net>
2025-10-21 00:45:52 -0700

    std blackparrot results

--- database/eval/blackparrot/export-to-json.sh
+++ database/eval/blackparrot/export-to-json.sh
@@ -5,5 +5,5 @@ TOP=$2
 
 #yosys -p "read_blif $NAME ; synth -noabc -flatten -top $TOP ; dfflegalize -cell \$_DFFE_PP_ 01 ; abc -g AND,OR,MUX ; proc ; write_json json/$TOP.json; tee -a stats/$TOP.dat stat -width"
 
-yosys -p "read_verilog -sv verilog/$NAME ; synth -flatten -top $TOP ; proc ; write_json json/$TOP.json; tee -a stats/$TOP.dat stat -width"
+yosys -p "read_verilog -sv $NAME ; synth -flatten -top $TOP ; proc ; write_json json/$TOP.json; tee -a stats/$TOP.dat stat -width"
 
