fbddc45ed7a38a21085a3eaffec2d05b533004a6
Zach Sisco <zach@zsisco.net>
2025-10-29 23:48:06 -0700

    rename dirs

--- memory-decomp/eval/collect_stats.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-for verilog in $@; do
-	echo "$verilog"
-
-	NAME=$(echo "$verilog" | cut -d . -f 1)
-	STATS=$NAME.dat
-	yosys -p "read_verilog $verilog ; hierarchy -auto-top ; proc; opt; clean -purge; write_rtlil $STATS ; tee -a $STATS stat -width"
-done
--- memory-decomp/run-bp.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-TOP=$1
-
-python3 memory_decomp.py --blif ~/bp/submods/${TOP}.blif --clock clk_i --top ${TOP} &> results/${TOP}.out
