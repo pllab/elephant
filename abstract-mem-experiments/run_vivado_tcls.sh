70623dcee0a929ba08d03fb16a0925cf463b88c1
Zach Sisco <zsisco@cuhk.edu.cn>
2026-03-18 17:57:09 +0800

    clean up experimental dirs

--- abstract-mem-experiments/run_vivado_tcls.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-for verilog in $@; do
-	vivado -mode tcl -source $verilog
-done
-
--- abstract-mem-experiments/rv-pyrtl/src/__init__.py
+++
@@ -1,3 +0,0 @@
-from .cpu import cpu, pipelined_cpu
-from .control import control
-from .program import Program
